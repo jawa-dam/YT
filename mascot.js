e91ab9236e899723f5b8fdccd795e85f710277f0
jawa-dam <seeklow@gmail.com>
2026-09-13 14:43:22 -0400

    V1.7.6: add shared Adam mascot system

---
+++ mascot.js
@@ -0,0 +1,12 @@
+(() => {
+  "use strict";
+
+  const MASCOT_URL = "https://assets.zyrosite.com/YZ9jg46Bljs5wOZR/yalltoo-mascot-animated-UgmkGIe3sJES4tKm.gif";
+
+  window.GEI_MASCOT = Object.freeze({
+    name: "Adam",
+    url: MASCOT_URL,
+    alt: "Adam, the YallToo mascot",
+    academyLabel: "Open Adam Academy guide"
+  });
+})();
